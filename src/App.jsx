b53aa39233ec43898bc81dd18e50a722bd4caab4
Angel de Jesus Ojeda Castro <angel.ojeda.castro@gmail.com>
2024-09-05 16:32:03 -0700

    added pokemon card and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,92 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <div className="header">
+          <div className="pokemon-name">Oshawott</div>
+          <div className="pokemon-id">#501</div>
+        </div>
+        <div className="image-container">
+          <div className="circular-progress">
+            <img
+              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/501.png"
+              alt="Oshawott"
+              width="200"
+            />
+          </div>
+        </div>
+        <div className="poke-info">
+          <div className="types-container">
+            <div className="type water">Water</div>
+          </div>
+          <div className="basics-info">
+            <div className="icon-color">
+              <i className="fas fa-weight" style={{marginRight: '5px'}}></i>
+              5.9 kg
+            </div>
+            <div className="icon-color">
+              <i className="fas fa-ruler-vertical" style={{marginRight: '5px'}}></i>
+              0.5m
+            </div>
+          </div>
+          <p className="description">
+            La vieira de su ombligo no solo sirve como arma, sino también como
+            instrumento para cortar las bayas que estén duras.
+          </p>
+          <div className="base-stats">
+            <h4 className="base-stats-title" >Base Stats</h4>
+            <div className="stat">
+              <span className="stat-info">HP</span>
+              <span className="stat-percentage">45</span>
+              <div className="stat-bar" >
+                <div className="stat-fill" style={{width: '45%'}}></div>
+              </div>
+            </div>
+            <div className="stat">
+              <span className="stat-info">ATK</span>
+              <span className="stat-percentage">49</span>
+              <div className="stat-bar">
+                <div className="stat-fill"  style={{width: '49%'}}></div>
+              </div>
+            </div>
+
+            <div className="stat">
+              <span className="stat-info">DEF</span>
+              <span className="stat-percentage">49</span>
+              <div className="stat-bar">
+                <div className="stat-fill"  style={{width: '49%'}}></div>
+              </div>
+            </div>
+
+            <div className="stat">
+              <span className="stat-info">SATK</span>
+              <span className="stat-percentage">65</span>
+              <div className="stat-bar">
+                <div className="stat-fill"  style={{width: '65%'}}></div>
+              </div>
+            </div>
+
+            <div className="stat">
+              <span className="stat-info">SDEF</span>
+              <span className="stat-percentage">65</span>
+              <div className="stat-bar">
+                <div className="stat-fill"  style={{width: '65%'}}></div>
+              </div>
+            </div>
+
+            <div className="stat">
+              <span className="stat-info">SPD</span>
+              <span className="stat-percentage">45</span>
+              <div className="stat-bar">
+                <div className="stat-fill"  style={{width: '65%'}}></div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
